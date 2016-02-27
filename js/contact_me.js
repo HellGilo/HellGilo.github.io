c17d3f0e5ce2e766f4b75eeb2d004aaccae314a3
HellGilo <hellgilo@gmail.com>
2016-02-28 00:50:53 +0100

    test

--- js/contact_me.js
+++ js/contact_me.js
@@ -30,14 +30,15 @@ $(function() {
                     'message': "You have received a new message from your website contact form.\n\n" + "Here are the details:\n\nName: "+name+"\n\nEmail: "+email+"\n\nPhone: "+phone+"\n\nMessage:\n" + message
                     },
                 cache: false,
-                success: function() {
+                success: function(e) {
+                    console.log(e);
                     // Enable button & show success message
                     $("#btnSubmit").attr("disabled", false);
                     $('#success').html("<div class='alert alert-success'>");
                     $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
                     $('#success > .alert-success')
-                        .append("<strong>Your message has been sent2. </strong>");
+                        .append("<strong>Your message has been sent. </strong>");
                     $('#success > .alert-success')
                         .append('</div>');
 
